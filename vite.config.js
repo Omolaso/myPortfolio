b64de332b195cc68592c76b4bba475612da5db76
omolaso <adextee001@gmail.com>
2023-11-09 02:48:02 +0100

    included build key

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-    server: {
-        port: 3000,
-        open: true
-    }
+	build: {
+		outDir: "build",
+	},
+	server: {
+		port: 3000,
+		open: true,
+	},
 });
